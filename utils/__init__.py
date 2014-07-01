9337ac9de6a921b9f454583dd82f8bd5340a5753
gkahiu <gkahiu@gmail.com>
2014-07-01 18:00:17 +0300

    Updates to document composer

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,4 @@ from filesize import size
 from filesize import traditional, alternative, verbose, iec, si
 from .reverse_dict import ReverseDict
 from .hashable_mixin import HashableMixin
+from .case_insensitive_dict import CaseInsensitiveDict
