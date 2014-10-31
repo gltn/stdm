847f25d8b2b227ec3481fd6dcf08db1f6dbbedc1
solomonN <njoroge.solomon@yahoo.com>
2014-10-31 11:09:44 +0300

    Issue with Report builder loading config tables resolved

--- third_party/__init__.py
+++ third_party/__init__.py
@@ -1,3 +1,17 @@
+from third_party.geraldo.base import ReportGroup, ReportBand, BAND_WIDTH
+from third_party.geraldo.utils import BAND_HEIGHT
+from third_party.geraldo.widgets import Label
 from xml2ddl.xml2ddl import *
 from xml2ddl import *
 from xml2ddl.xml2html import *
+from third_party.geoalchemy2.types import *
+from geoalchemy2.types import Geometry
+from reportlab.lib import *
+from geraldo import  (
+ObjectValue,
+    Label,
+    ReportGroup
+)
+import sqlalchemy
+import ttfquery
+import third_party.FontTools
