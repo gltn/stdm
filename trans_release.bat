231e3011623088745e783b1aa6d1def558acf279
solomonN <njoroge.solomon@yahoo.com>
2015-06-08 09:21:59 +0300

    Layout Error when generating reports in French version of STDM fixed.

--- trans_release.bat
+++ trans_release.bat
@@ -1,3 +1,3 @@
 REM Script for updating the STDM translation sections throughout the application
-cd C:\Program Files (x86)\Quantum GIS Lisboa\apps\qgis\python\plugins\stdm
+cd %~dp0
 lrelease stdm.pro
