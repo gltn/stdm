75d6371eef8f8170a078abb9005ac3ea4b2a68d5
John Gitau <gkahiu@gmail.com>
2016-04-24 10:37:54 +0300

    Fix issue with saving database connections to the registry

--- data/config.py
+++ data/config.py
@@ -49,7 +49,7 @@ class DatabaseConfig(object):
         db_settings = {}
         db_settings[self.host] = db_connection.Host
         db_settings[self.port] = db_connection.Port
-        db_settings[self.dbname] = db_connection.Database
+        db_settings[self.db_name] = db_connection.Database
 
         self.reg_config.write(db_settings)
     
