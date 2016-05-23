870a9c67f77af6211cca051ea289d0323b3f4048
John Gitau <gkahiu@gmail.com>
2016-05-23 14:30:55 +0300

    Modify server settings in Login dialog.

--- ui/ui_login.py
+++ ui/ui_login.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'ui_login.ui'
 #
-# Created: Mon Sep 01 12:03:21 2014
-#      by: PyQt4 UI code generator 4.11.1
+# Created: Mon May 23 14:10:34 2016
+#      by: PyQt4 UI code generator 4.9.4
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,24 +12,29 @@ from PyQt4 import QtCore, QtGui
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
 except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+    _fromUtf8 = lambda s: s
 
 class Ui_frmLogin(object):
     def setupUi(self, frmLogin):
         frmLogin.setObjectName(_fromUtf8("frmLogin"))
-        frmLogin.resize(320, 208)
-        frmLogin.setMaximumSize(QtCore.QSize(320, 16777215))
+        frmLogin.resize(320, 152)
+        frmLogin.setMaximumSize(QtCore.QSize(320, 152))
         self.gridLayout_2 = QtGui.QGridLayout(frmLogin)
         self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
+        self.btnBox = QtGui.QDialogButtonBox(frmLogin)
+        self.btnBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
+        self.btnBox.setObjectName(_fromUtf8("btnBox"))
+        self.gridLayout_2.addWidget(self.btnBox, 3, 1, 1, 1)
+        self.btn_db_settings = QtGui.QToolButton(frmLogin)
+        self.btn_db_settings.setText(_fromUtf8(""))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/plugins/stdm/images/icons/db_server_settings.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.btn_db_settings.setIcon(icon)
+        self.btn_db_settings.setIconSize(QtCore.QSize(16, 16))
+        self.btn_db_settings.setCheckable(False)
+        self.btn_db_settings.setAutoRaise(False)
+        self.btn_db_settings.setObjectName(_fromUtf8("btn_db_settings"))
+        self.gridLayout_2.addWidget(self.btn_db_settings, 3, 0, 1, 1)
         self.gridLayout = QtGui.QGridLayout()
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
         self.txtUserName = QtGui.QLineEdit(frmLogin)
@@ -48,14 +53,10 @@ class Ui_frmLogin(object):
         self.txtPassword.setEchoMode(QtGui.QLineEdit.Password)
         self.txtPassword.setObjectName(_fromUtf8("txtPassword"))
         self.gridLayout.addWidget(self.txtPassword, 1, 1, 1, 1)
-        self.gridLayout_2.addLayout(self.gridLayout, 1, 0, 1, 1)
-        self.btnBox = QtGui.QDialogButtonBox(frmLogin)
-        self.btnBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
-        self.btnBox.setObjectName(_fromUtf8("btnBox"))
-        self.gridLayout_2.addWidget(self.btnBox, 2, 0, 1, 1)
+        self.gridLayout_2.addLayout(self.gridLayout, 1, 0, 1, 2)
         self.vlNotification = QtGui.QVBoxLayout()
         self.vlNotification.setObjectName(_fromUtf8("vlNotification"))
-        self.gridLayout_2.addLayout(self.vlNotification, 0, 0, 1, 1)
+        self.gridLayout_2.addLayout(self.vlNotification, 0, 0, 1, 2)
 
         self.retranslateUi(frmLogin)
         QtCore.QObject.connect(self.btnBox, QtCore.SIGNAL(_fromUtf8("rejected()")), frmLogin.reject)
@@ -64,7 +65,11 @@ class Ui_frmLogin(object):
         frmLogin.setTabOrder(self.txtPassword, self.btnBox)
 
     def retranslateUi(self, frmLogin):
-        frmLogin.setWindowTitle(_translate("frmLogin", "STDM Login", None))
-        self.label.setText(_translate("frmLogin", "UserName", None))
-        self.label_2.setText(_translate("frmLogin", "Password", None))
+        frmLogin.setWindowTitle(QtGui.QApplication.translate("frmLogin", "STDM Login", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_db_settings.setToolTip(QtGui.QApplication.translate("frmLogin", "Edit database server settings", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_db_settings.setStatusTip(QtGui.QApplication.translate("frmLogin", "Edit database server settings", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_db_settings.setWhatsThis(QtGui.QApplication.translate("frmLogin", "Edit database server settings", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("frmLogin", "UserName", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_2.setText(QtGui.QApplication.translate("frmLogin", "Password", None, QtGui.QApplication.UnicodeUTF8))
 
+import resources_rc
