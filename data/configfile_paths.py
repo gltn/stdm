24fe15c0608e9f52062545c9c57e68e124229d35
eomwandho <e.omwandho@gmail.com>
2016-06-06 11:09:35 +0300

    removed config file loading from template folder, added file updater, added configuration .stc to template folder and reorganize config loading

--- data/configfile_paths.py
+++ data/configfile_paths.py
@@ -23,7 +23,7 @@ import os
 import shutil
 import platform
 import filecmp
-from stdm.utils.util import PLUGIN_DIR
+from ..utils.util import PLUGIN_DIR
 from PyQt4.QtGui import QApplication, QMessageBox
 DEFAULT_CONFIG="stdmConfig.xml"
 LICENSE="LICENSE.txt"
@@ -34,7 +34,7 @@ HELP="stdm.chm"
 
 xmldoc= os.path.dirname(os.path.abspath(__file__))
 
-from stdm.settings.registryconfig import RegistryConfig
+from ..settings.registryconfig import RegistryConfig
 from .reports import SysFonts
 
 class FilePaths(object):
@@ -65,7 +65,8 @@ class FilePaths(object):
         return self._file
 
     def cacheFile(self):
-        #To implemented a backup file for comparing edits everytime the user makes changes
+        # To implemented a backup file for comparing edits everytime the user
+        #  makes changes
         path = self.userPath+'/temp/%s'%DEFAULT_CONFIG
         return path
     
@@ -106,7 +107,8 @@ class FilePaths(object):
         """
         returns the path with base configuration file
         """
-        self.baseDir = self._file+"/template/"       
+        self.baseDir = self._file+"/template/"
+        return self.baseDir
     
     def setUserConfigPath(self,path=None):
         ''' set new path with user configuration'''
@@ -120,10 +122,10 @@ class FilePaths(object):
         self.userConfigPath(self.userPath)
     
     def userConfigPath(self,path=None):
-        #Copy template files to the user directory
+        # Copy template files to the user directory
         try:
-            #self.compare_config_version(FILE)
-            for fileN in [DEFAULT_CONFIG, BASIC_SQL]:
+            # self.compare_config_version(FILE)
+            for fileN in [BASIC_SQL]:
                 if not os.path.isfile(path+'/%s'%fileN):
                     baseFile = self.baseDir +'/%s'%fileN
                     shutil.copy(baseFile,self.userPath)
@@ -238,4 +240,9 @@ class FilePaths(object):
             shutil.copy(base_file, self.userPath)
         except:
             pass
+
+    def get_configuration_file(self):
+        """ Default path to the config file """
+        xml = self.userPath +'/%s'%DEFAULT_CONFIG
+        return xml
 
