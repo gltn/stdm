59b6102594e914bbc7ac3194f21c73639591ce10
eomwandho <e.omwandho@gmail.com>
2016-06-08 17:55:31 +0300

    added method to write new config file

--- settings/config_file_updater.py
+++ settings/config_file_updater.py
@@ -1,7 +1,8 @@
 """
 /***************************************************************************
-Name                 : ConfigurationWriter
-Description          : Reads/writes configuration object from/to file.
+Name                 : Configuration File Updater and Backup
+Description          : Updates and backups up the content of either old
+                        configuration or new configuration
 Date                 : 15/February/2016
 copyright            : (C) 2015 by UN-Habitat and implementing partners.
                        See the accompanying file CONTRIBUTORS.txt in the root
@@ -21,7 +22,9 @@ email                : stdm@unhabitat.org
 import os
 import shutil
 
-from PyQt4.QtCore import QFile, QIODevice
+from collections import OrderedDict
+
+from PyQt4.QtCore import QFile, QIODevice, QTextStream
 from PyQt4.QtGui import QMessageBox
 from PyQt4.QtXml import QDomDocument
 
@@ -38,9 +41,10 @@ class ConfigurationFileUpdater(object):
     def __init__(self):
         self.file_handler = FilePaths()
         self.version = None
-        self.profiles = []
-        self.table_dict = {}
+        self.config_profiles = []
+        self.table_dict = OrderedDict()
         self.table_list = []
+        self.config_file = None
 
     def _check_config_folder_exists(self):
         """
@@ -72,8 +76,7 @@ class ConfigurationFileUpdater(object):
             return False
 
     def _get_doc_root(self, path):
-        config_file = os.path.join(self.file_handler.localPath(),
-                                       path)
+        config_file = os.path.join(self.file_handler.localPath(), path)
         config_file = QFile(config_file)
         if not config_file.open(QIODevice.ReadOnly):
             raise IOError('Cannot read configuration file. Check read '
@@ -99,7 +102,8 @@ class ConfigurationFileUpdater(object):
         if config_version:
             config_version = float(config_version)
         else:
-            #Fatal error
+
+            # Fatal error
             raise ConfigurationException('Error extracting version '
                                          'number from the '
                                          'configuration file.')
@@ -110,10 +114,11 @@ class ConfigurationFileUpdater(object):
 
     def _copy_config_file_from_template(self):
         shutil.copy(os.path.join(self.file_handler.defaultConfigPath(),
-                                     "configuration.stc"),
-                        self.file_handler.localPath())
+                                 "configuration.stc"),
+                    self.file_handler.localPath())
 
     def _set_table_columns(self, table_name, element):
+
         for i in range(element.count()):
             column_dict = {}
             columns_node = element.item(i).toElement()
@@ -125,7 +130,8 @@ class ConfigurationFileUpdater(object):
                     column_dict["col_name"] = col_name
                     col_search = unicode(column_node.attribute('searchable'))
                     column_dict["col_search"] = col_search
-                    col_description = unicode(column_node.attribute('fullname'))
+                    col_description = unicode(
+                        column_node.attribute('fullname'))
                     column_dict["col_descrpt"] = col_description
                     col_type = unicode(column_node.attribute('type'))
                     column_dict["col_type"] = col_type
@@ -133,6 +139,7 @@ class ConfigurationFileUpdater(object):
         self.table_dict[table_name] = self.table_list
 
     def _set_table_attributes(self, element):
+
         for i in range(element.count()):
             profile_child_node = element.item(i).toElement()
             if profile_child_node.tagName() == "table":
@@ -151,32 +158,65 @@ class ConfigurationFileUpdater(object):
                 pass
 
     def _set_version_profile(self, element):
+        """
+        Internal function to load version and profile to dictionary
+        """
         for i in range(element.count()):
             child_node = element.item(i).toElement()
             if child_node.hasAttribute('version'):
                 self.version = unicode(child_node.attribute('version'))
             if child_node.tagName() == "profile":
-                self.profiles.append(unicode(child_node.attribute('name')))
+                self.config_profiles.append(unicode(child_node.attribute('name')))
                 profile_child_nodes = child_node.childNodes()
                 self._set_table_attributes(profile_child_nodes)
 
+    def _create_config_file(self, config_file_name):
+        self.config_file = QFile(os.path.join(self.file_handler.localPath(),
+                                   config_file_name))
+
+        if not self.config_file.open(QFile.WriteOnly | QFile.Text):
+            QMessageBox.warning(None, "Config Error", "Cannot write file {"
+                                "0}: \n {2}".format(
+                                 self.config_file.fileName(),
+                                 self.config_file.errorString()))
+        return
+
     def load(self):
         if self._check_config_folder_exists():
+
             # Check if old configuration file exists
             if self._check_config_file_exists("stdmConfig.xml"):
                 root = self._get_doc_root(os.path.join(
                     self.file_handler.localPath(), "stdmConfig.xml"))
-                node_list = []
                 child_nodes = root.childNodes()
+
+                # Parse old configuration to dictionary
                 self._set_version_profile(child_nodes)
 
-                QMessageBox.information(None, "Test", str(
-                    self.table_dict))
+                # Create config file
+                self._create_config_file("configuration.stc")
+                doc = QDomDocument()
+                configuration = doc.createElement("Configuration")
+                configuration.setAttribute("version", self.version)
+
+                for config_profile in self.config_profiles:
+                    profile = doc.createElement("Profile")
+                    profile.setAttribute("description", "")
+                    profile.setAttribute("name", config_profile)
+                    configuration.appendChild(profile)
+
+                doc.appendChild(configuration)
+
+                stream = QTextStream(self.config_file)
+                stream << doc.toString()
+
                 return True
             else:
+                # Check of new config format exists
                 if self._check_config_file_exists("configuration.stc"):
                     return True
                 else:
+                    # if new config format doesn't exist copy from template
                     self._copy_config_file_from_template()
                     return True
         else:
