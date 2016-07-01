7ba76d71895691c7c0f9939a55b9034694e46552
eomwandho <e.omwandho@gmail.com>
2016-07-01 19:00:15 +0300

    latest changes

--- plugin.py
+++ plugin.py
@@ -502,6 +502,18 @@ class STDMQGISLoader(object):
         """
         if self.configuration_file_updater.load():
 
+            if self.configuration_file_updater.check_version():
+                return True
+            else:
+                if QMessageBox.information(None, "Update STDM Config",
+                                        "Do you want to backup your data?",
+                                            QMessageBox.Yes |
+                                        QMessageBox.No) == QMessageBox.Yes:
+                    pass
+                else:
+                    # Update .stc version to stdm instance version
+                    return True
+
             try:
                 self.config_serializer.load()
             except IOError as io_err:
@@ -522,18 +534,6 @@ class STDMQGISLoader(object):
                 )
 
                 return False
-
-            if self.configuration_file_updater.check_version():
-                return True
-            else:
-                if QMessageBox.information(None, "Update STDM Config",
-                                        "Do you want to backup your data?",
-                                            QMessageBox.Yes |
-                                        QMessageBox.No) == QMessageBox.Yes:
-                    pass
-                else:
-                    # Update .stc version to stdm instance version
-                    return True
 
         else:
             return False
