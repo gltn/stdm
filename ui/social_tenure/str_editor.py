7af887f66dcfbde948ff427999f2159e22a6cd86
Wondiamgegn Tesfaye <wondim81@gmail.com>
2017-01-18 21:22:31 +0300

    Commented and cleaned up code

--- ui/social_tenure/str_editor.py
+++ ui/social_tenure/str_editor.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+Name                 : STR Editor
+Description          : GUI classes for Social tenure editor.
+Date                 : 10/November/2016
+copyright            : (C) 2016 by UN-Habitat and implementing partners.
+                       See the accompanying file CONTRIBUTORS.txt in the root
+email                : stdm@unhabitat.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
 from collections import OrderedDict
 
 from PyQt4.QtCore import QRegExp
