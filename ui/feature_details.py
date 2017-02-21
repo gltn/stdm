457a7c899217a9c053c9a4823c5eee1fb01e3c86
Wondiamgegn Tesfaye <wondim81@gmail.com>
2017-02-22 01:34:35 +0300

    Minor change

--- ui/feature_details.py
+++ ui/feature_details.py
@@ -473,7 +473,6 @@ class DetailsDockWidget(QDockWidget, Ui_DetailsDock, LayerSelectionHandler):
         clearing feature selection, and hiding the dock.
         :param tool: Feature detail tool button
         :type tool: QAction
-        :return: None
         """
         self.iface.actionPan().trigger()
         tool.setChecked(False)
