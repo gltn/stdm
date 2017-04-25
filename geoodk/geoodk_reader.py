ce471fdd683100ed7026c67025b1850b2aebd64c
solomonN <njoroge.solomon@yahoo.com>
2017-04-25 21:45:50 +0300

    updates to geoodk reader

--- geoodk/geoodk_reader.py
+++ geoodk/geoodk_reader.py
@@ -4,35 +4,30 @@
 from stdm.settings import current_profile
 
 
+
 def read():
     """
-
+    Test implementation hand codes the entity but would need to be
+    implemented on the dialog for user to make their own selection
     :param self:
     :return:dict of entity column and data type
     """
-    profile_read = current_profile()
-    return profile_read
-
-def read1():
-    """
-
-    :param self:
-    :return:dict of entity column and data type
-    """
-    '''print vars(current_profile())'''
     entity_prop = {}
-    profile_read = current_profile()
-    cols = profile_read.entity_by_name("lo_parcel").columns
+    profile_reader = current_profile()
+    cols = profile_reader.entity_by_name("lo_parcel").columns
     col_objs =cols.values()
     for obj in col_objs:
         entity_prop[obj.name] = obj.TYPE_INFO
     return entity_prop
 
-def select_entity():
+def read_lookup_for_column(col, obj):
     """
-
+    Check if the column has a lookup and get the associated lookup definition
     :return:
     """
-    profile_read = current_profile()
-    entity = profile_read.entity_by_name("lo_parcel")
-    return entity
+    if isinstance(col, obj):
+        if col.TYPE_INFO == "LOOKUP":
+            return
+
+
+
