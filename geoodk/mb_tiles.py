54ee41eec82027a1d238b4acdedef6a61b77a800
solomonN <njoroge.solomon@yahoo.com>
2017-04-26 14:38:15 +0300

    added file for converting to mbtiles

---
+++ geoodk/mb_tiles.py
@@ -0,0 +1,13 @@
+"""
+
+"""
+from gdal import gdal_translate
+
+def convert_to_mbtiles(input_file, outputfile):
+    """
+    method to convert gdal file(TIFF) to MBTiles
+    :param self:
+    :return:file
+    """
+    return gdal_translate(input_file, outputfile)
+
