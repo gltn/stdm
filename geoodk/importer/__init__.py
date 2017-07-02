bf0b5224775db7fc6254795b73a728494e29196b
solomonN <njoroge.solomon@yahoo.com>
2017-07-03 01:51:50 +0300

    Merge remote-tracking branch 'origin/stdm_geoodk' into stdm_geoodk
    # Conflicts:
    #	geoodk/geoodk_writer.py
    #	geoodk/importer/__init__.py
    #	geoodk/importer/entity_importer.py
    #	geoodk/xform_model.py
    #	plugin.py
    #	resources.qrc
    #	resources_rc.py
    #	ui/geoodk_converter_dialog.py
    #	ui/ui_geoodk_converter.ui
    #	utils/util.py

--- geoodk/importer/__init__.py
+++ geoodk/importer/__init__.py
@@ -1,7 +1,4 @@
 from entity_importer import EntityImporter, Save2DB
 from uuid_extractor import InstanceUUIDExtractor
-<<<<<<< HEAD
 from geometry_provider import GeometryProvider
 from geometry_provider import GeomPolgyon
-=======
->>>>>>> origin/stdm_geoodk
