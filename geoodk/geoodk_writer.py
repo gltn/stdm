19a4e674e959298db0a9d39a8e6ccb4f03296d7c
solomonN <njoroge.solomon@yahoo.com>
2017-11-29 23:51:38 +0300

    added support for decode and encoding characters

--- geoodk/geoodk_writer.py
+++ geoodk/geoodk_writer.py
@@ -786,11 +786,12 @@ class GeoodkWriter(EntityFormatter, XFORMDocument):
 
             lk_item = self.create_node("item")
             lk_item_label = self.create_node("label")
-            lk_item_label_txt = self.create_text_node(key)
+            decoded_key = key.encode('utf_8')
+            lk_item_label_txt = self.create_text_node(decoded_key)
             lk_item_label_txt_val = self.create_node("value")
             if val == "":
                 val = key
-            lk_item_label_txt_val_txt = self.create_text_node(val)
+            lk_item_label_txt_val_txt = self.create_text_node(val.encode('utf_8'))
 
             lk_item_label.appendChild(lk_item_label_txt)
             lk_item_label_txt_val.appendChild(lk_item_label_txt_val_txt)
