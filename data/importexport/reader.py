7670046c2066161c7c580da1816d8a87024d1d47
Wondimagegn Tesfaye Beshah <wondim81@gmail.com>
2018-04-03 02:40:51 +0300

    Moved dummy column creation to plugin.py

--- data/importexport/reader.py
+++ data/importexport/reader.py
@@ -157,15 +157,6 @@ class OGRReader(object):
         float_type = ['DOUBLE', 'PERCENT']
         int_type = ['INT', 'LOOKUP', 'ADMIN_SPATIAL_UNIT',
                     'FOREIGN_KEY']
-        if col_name not in entity.columns.keys():
-            try:
-                if value is not None:
-                    value = int(value)
-                    if isinstance(value, int):
-                        if value == 0:
-                            value = None
-            except ValueError:
-                value = None
 
         if col_name in entity.columns.keys():
             if entity.columns[col_name].TYPE_INFO in integer_types:
@@ -209,8 +200,7 @@ class OGRReader(object):
         entity = self._data_source_entity(target_table)
 
         date_types = ['DATE', 'DATETIME']
-        if col_name not in entity.columns.keys():
-            return value
+
         if entity.columns[col_name].TYPE_INFO in date_types:
             if not bool(value) or value.lower() == 'null':
                 value = None
@@ -231,8 +221,7 @@ class OGRReader(object):
         """
         entity = self._data_source_entity(target_table)
         yes_no_types = ['BOOL']
-        if col_name not in entity.columns.keys():
-            return value
+       
         if entity.columns[col_name].TYPE_INFO in yes_no_types:
             if not bool(value.strip()) or value.strip().lower() == 'null':
                 value = None
