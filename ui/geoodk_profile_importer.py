828527060b52bede2078b1d060cf6bc1d41b4404
Paul Gathogo <pgathogo@gmail.com>
2018-10-12 16:09:51 +0300

    Refactoring Geoodk profile importer

--- ui/geoodk_profile_importer.py
+++ ui/geoodk_profile_importer.py
@@ -215,7 +215,6 @@ class ProfileInstanceRecords(QDialog, FORM_CLASS):
         Extract the specific folder information and rename the file
         :return:
         """
-        import pydevd; pydevd.settrace()
         self.uuid_extractor.new_list = []
         if self.record_count() > 0:
             directories = self.xform_xpaths()
