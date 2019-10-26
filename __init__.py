23c284a7354ed73072f94978a0d6646f7b2edf62
SOLOMON <njoroge.solomons@gmail.com>
2019-10-27 00:34:49 +0300

    Fixed error when generating mobile forms caused by show in parent function.

--- __init__.py
+++ __init__.py
@@ -31,7 +31,9 @@ from PyQt4.QtGui import (
 )
 from PyQt4.QtCore import (
     QDir,
-    QFile
+    QFile,
+    QTextStream,
+    QIODevice
 )
 
 #Load third party libraries
@@ -93,29 +95,49 @@ def copy_core_configuration():
     """
     Copies the basic STDM configuration to the user directory if there is none.
     """
-    core_config_path = u'{0}/templates/configuration.stc'.format(
-        os.path.dirname(__file__)
-    )
 
-    #Exit if the core configuration does not exist
+    #check network config exist
+    core_config_path = read_network_path()
     if not QFile.exists(core_config_path):
         return
+    else:
 
-    #File name of previous configuration
-    v1_1_config_path = u'{0}/stdmConfig.xml'.format(USER_PLUGIN_DIR)
+    # if not core_config_path:
+    #     core_config_path = u'{0}/templates/configuration.stc'.format(
+    #         os.path.dirname(__file__)
+    #     )
+    #
+    #     #Exit if the core configuration does not exist
+    #     if not QFile.exists(core_config_path):
+    #         return
 
-    #Only copy the new one if there is no copy of the previous version
-    # since the version updater will automatically handle the upgrade.
-    if QFile.exists(v1_1_config_path):
-        #Version update will handle the migration
-        return
+        #File name of previous configuration
+        v1_1_config_path = u'{0}/stdmConfig.xml'.format(USER_PLUGIN_DIR)
 
-    #Copy config assuming that the plugin user folder has no previous
-    # configuration.
-    conf_file = QFile(core_config_path)
-    conf_dest = u'{0}/configuration.stc'.format(USER_PLUGIN_DIR)
+        #Only copy the new one if there is no copy of the previous version
+        # since the version updater will automatically handle the upgrade.
+        if QFile.exists(v1_1_config_path):
+            #Version update will handle the migration
+            return
 
-    copy_status = conf_file.copy(conf_dest)
+        #Copy config assuming that the plugin user folder has no previous
+        # configuration.
+        conf_file = QFile(core_config_path)
+        conf_dest = u'{0}/configuration.stc'.format(USER_PLUGIN_DIR)
+
+        copy_status = conf_file.copy(conf_dest)
+
+
+def read_network_path():
+    """get the netowrk config file location"""
+    network_config = QFile(os.path.join(USER_PLUGIN_DIR,
+                                        'network_path.txt'))
+
+    if network_config.open(QIODevice.ReadOnly):
+        stream = QTextStream(network_config)
+        f_path = stream.readLine()
+        if f_path:
+            return f_path
 
 
 def classFactory(iface):
