2f0f2ab950eb125da3d09067f0159a99cd834231
nkoech <koechnicholas@gmail.com>
2019-12-30 04:00:08 +0300

    Added methods

--- ui/flts/workflow_manager/plot.py
+++ ui/flts/workflow_manager/plot.py
@@ -36,12 +36,13 @@ class PlotFile:
         self._data_service = data_service
         self._fpath = None
         self._fpaths = []
+        self._delimiters = {",": "Comma", ";": "Semicolon", "\t": "Tab"}
 
     def set_file_path(self, fpath):
         """
         Sets plot import file absolute path
         :param fpath: Plot import file absolute path
-        :rtype fpath: String
+        :type fpath: String
         """
         self._fpath = fpath
 
@@ -80,14 +81,21 @@ class PlotFile:
     #     """
     #     return ["Plots", "Beacons", "Servitudes", "Field Book"]
 
-    @staticmethod
-    def delimiters():
+    def delimiters(self):
         """
         Returns delimiters
         :return: Delimiters
+        :rtype: Dictionary
+        """
+        return self._delimiters
+
+    def get_row_data(self):
+        """
+        File properties method wrapper
+        :return: Plot import file data properties
         :rtype: List
         """
-        return OrderedDict({",": "Comma", ";": "Semicolon", "\t": "Tab"})
+        return self._file_properties(self._fpath)
 
     def load(self):
         """
@@ -103,39 +111,37 @@ class PlotFile:
         except(IOError, OSError, Exception) as e:
             raise e
 
-    def get_row_data(self):
-        """
-        File properties method wrapper
-        :return: Plot import file data properties
-        :rtype: List
-        """
-        return self._file_properties(self._fpath)
-
     def _file_properties(self, fpath):
         """
         Returns plot import file data properties
         :param fpath: Plot import file absolute path
-        :rtype fpath: String
+        :type fpath: String
         :return results: Plot import file data properties
         :return results: List
         """
         results = []
         properties = {}
         try:
+            header_row = 1
             file_extension = QFileInfo(fpath).completeSuffix()
-            delimiter = self._get_delimiter(fpath)
+            delimiter = self._get_csv_delimiter(fpath)
+            self.set_delimiter(delimiter)
             for n, prop in enumerate(self._data_service.columns):
                 if prop.name == "Name":
                     properties[n] = QFileInfo(fpath).fileName()
                 elif prop.name == "Import as":
                     properties[n] = unicode(self._get_import_type(fpath))
                 elif prop.name == "Delimiter":
-                    properties[n] = unicode(delimiter)
+                    name = "{0} ( {1} )".format(self._delimiters[delimiter], delimiter) if delimiter else ""
+                    properties[n] = unicode(name)
                 elif prop.name == "Header row":
-                    properties[n] = float(1) if file_extension != "pdf" else ""
+                    header_row = 1
+                    properties[n] = float(header_row) if file_extension != "pdf" else unicode("")
                 elif prop.name == "Geometry field":
-                    # Get possible header row
-                    properties[n] = unicode("Test")
+                    field_names = self.get_csv_fields(
+                        fpath, header_row - 1, delimiter
+                    )
+                    properties[n] = unicode(field_names[0]) if field_names else unicode("")
                 else:
                     properties[n] = unicode("")
         except (csv.Error, Exception) as e:
@@ -144,34 +150,40 @@ class PlotFile:
         results.append(properties)
         return results
 
-    def _get_delimiter(self, fpath):
+    @staticmethod
+    def _get_csv_delimiter(fpath):
         """
         Returns default plain text common delimiter
         :param fpath: Plot import file absolute path
-        :rtype fpath: String
+        :type fpath: String
         :return: Default common delimiter
         :rtype: Unicode
         """
         file_extension = QFileInfo(fpath).completeSuffix()
         if file_extension not in ("csv", "txt"):
-            return ""
+            return
         try:
             with open(fpath, 'r') as csv_file:
                 dialect = csv.Sniffer().sniff(csv_file.readline(4096))
-                delimiter = dialect.delimiter
-                options = self.delimiters()
-                if delimiter in options.keys():
-                    return options[delimiter]
-                return delimiter
+                return dialect.delimiter
         except (csv.Error, Exception) as e:
             raise e
+
+    def set_delimiter(self, delimiter):
+        """
+        Sets custom/new delimiter
+        :param delimiter: delimiter
+        :type delimiter: String
+        """
+        if delimiter and delimiter not in self._delimiters.keys():
+            self._delimiters[delimiter] = "Custom"
 
     @staticmethod
     def _get_import_type(fpath):
         """
         Returns default import type based on file extension
         :param fpath: Plot import file absolute path
-        :rtype fpath: String
+        :type fpath: String
         :return: Default import type
         :rtype: String
         """
@@ -180,6 +192,30 @@ class PlotFile:
             return "Field Book"
         return "Plots"
 
+    @staticmethod
+    def get_csv_fields(fpath, row, delimiter=None):
+        """
+        Returns plain text field names
+        :param fpath: Plot import file absolute path
+        :type fpath: String
+        :param row: Header row number
+        :type row: Integer
+        :param delimiter: Delimiter
+        :type delimiter: String
+        :return: CSV field names
+        :rtype: List
+        """
+        file_extension = QFileInfo(fpath).completeSuffix()
+        if file_extension not in ("csv", "txt"):
+            return
+        try:
+            with open(fpath, 'r') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=delimiter)
+                csv_reader = list(csv_reader)
+                return csv_reader[row]
+        except (csv.Error, Exception) as e:
+            raise e
+
     def get_headers(self):
         """
         Returns column label configurations
@@ -187,5 +223,4 @@ class PlotFile:
         :rtype: List
         """
         return self._data_service.columns
-
 
