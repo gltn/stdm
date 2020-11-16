cd51a688aa033940ca01cf3b1315bbccb0a6d2e8
Nyall Dawson <nyall.dawson@gmail.com>
2020-11-16 15:42:23 +1000

    Upgrade about dialog

--- stdm/ui/frmAbout.py
+++ stdm/ui/frmAbout.py
@@ -18,33 +18,32 @@ email                : gkahiu@gmail.com
 """
 import os
 
-from qgis.PyQt.QtGui import (
-    QPixmap
-)
+from qgis.PyQt import uic
+
 from qgis.PyQt.QtWidgets import (
     QDialog
 )
 
-from stdm.ui.ui_about_stdm import Ui_frmAbout
+from stdm.ui.gui_utils import GuiUtils
 
+WIDGET, BASE = uic.loadUiType(
+    GuiUtils.get_ui_file_path('ui_about_stdm.ui'))
 
-class AboutDialog(QDialog):
+class AboutDialog(WIDGET, BASE):
 
     # Class constructor
     def __init__(self, photoArray=None):
         QDialog.__init__(self)
-        # Inherit from base UI class
-        self.ui = Ui_frmAbout()
         # Call the initialize component method
-        self.ui.setupUi(self)
+        self.setupUi(self)
         # Resources have been embedded
         # self.initDialog()
-        self.ui.buttonBox.clicked.connect(self.close)
+        self.buttonBox.clicked.connect(self.close)
 
     def initDialog(self):
         # Fetch references
-        logoPixMap = QPixmap(":/plugins/stdm/images/un_habitat.jpg")
-        self.ui.lblLogo.setPixmap(logoPixMap)
+        logoPixMap = GuiUtils.get_icon_pixmap("un_habitat.jpg")
+        self.lblLogo.setPixmap(logoPixMap)
         dirP = os.path.dirname(__file__)
         normPath = str(dirP) + '\stdm\summary.txt'
         # absPath=os.path.join(str(dir),'\stdm\summary.txt')
